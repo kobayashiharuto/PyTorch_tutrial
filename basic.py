501f5d431690bde95f3d5047a94c1441ab2a21c5
kl <datesaver1234@gmail.com>
2021-07-20 01:33:03 +0900

    loss

--- basic.py
+++ basic.py
@@ -4,24 +4,23 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
-        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc1 = nn.Linear(in_features=16*5*5, out_features=120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, (2, 2))
-        x = F.relu(self.conv2)
+        x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, (2, 2))
         x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1)
-        x = F.relu(self.fc2)
-        x = self.fc3
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
     def num_flat_features(self, x):
@@ -30,3 +29,14 @@ class Net(nn.Module):
         for s in size:
             num_features *= s
         return num_features
+
+
+net = Net()
+print(net)
+input = torch.randn(1, 1, 32, 32, requires_grad=True)
+output = net(input)
+target = torch.randn(10)
+target = target.view(1, -1)
+criterion = nn.MSELoss()
+loss = criterion(output, target)
+print(loss)
