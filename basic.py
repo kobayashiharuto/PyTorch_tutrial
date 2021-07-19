6f7dc3603dd96f02e5f9a23a70b496f87668423e
kl <datesaver1234@gmail.com>
2021-07-20 00:45:54 +0900

    model

--- basic.py
+++ basic.py
@@ -1,7 +1,32 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-t = torch.tensor([1, 2])
-t = t.unsqueeze(0)
-print(t)
-t = t.squeeze()
-print(t)
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, (2, 2))
+        x = F.relu(self.conv2)
+        x = F.max_pool2d(x, (2, 2))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1)
+        x = F.relu(self.fc2)
+        x = self.fc3
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
